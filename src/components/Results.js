f1f6bc753e55532289fd1052075569b500e92375
codyarose <crose992@gmail.com>
2019-08-02 15:54:16 -0400

    Refactoring Results into own component file

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,45 +1,47 @@
 import React, { useState } from 'react'
-import Async from 'react-async'
-import { API } from '../utils/api'
+// import Async from 'react-async'
+// import { API } from '../utils/api'
 import { Search } from './Search'
-import { SimpleModal } from './Modal'
+import { ResultsNew } from './Results-new'
+// import { SimpleModal } from './Modal'
 
 export const Results = () => {
 	const [query, setQuery] = useState('')
 
-	const loadMovies = () =>
-	fetch(`https://api.themoviedb.org/3/search/movie?api_key=${ API }&language=en-US&query=${ query }&page=1&include_adult=false`)
-		.then(res => (res.ok ? res : Promise.reject(res)))
-		.then(res => res.json())
+	// const loadMovies = () =>
+	// fetch(`https://api.themoviedb.org/3/search/movie?api_key=${ API }&language=en-US&query=${ query }&page=1&include_adult=false`)
+	// 	.then(res => (res.ok ? res : Promise.reject(res)))
+	// 	.then(res => res.json())
 
 	return(
 		<div>
 			<Search setValue={setQuery} />
 			{query &&
-				<Async promiseFn={loadMovies}>
-					<Async.Loading>Loading...</Async.Loading>
-					<Async.Fulfilled>
-						{data => {
-							return (
-								data.results.map(result => (
-									<div key={result.id}>
-										{result.poster_path &&
-											<img src={`https://image.tmdb.org/t/p/w200${result.poster_path}`} alt=""/>
-										}
-										<h2>{result.title}</h2>
-										<SimpleModal>
-											<h3>{result.title}</h3>
-											<p>{result.overview}</p>
-										</SimpleModal>
-									</div>
-								))
-							)
-						}}
-					</Async.Fulfilled>
-					<Async.Rejected>
-						{error => `Something went wrong: ${error.message}`}
-					</Async.Rejected>
-				</Async>
+				<ResultsNew query={query} />
+				// <Async promiseFn={loadMovies}>
+				// 	<Async.Loading>Loading...</Async.Loading>
+				// 	<Async.Fulfilled>
+				// 		{data => {
+				// 			return (
+				// 				data.results.map(result => (
+				// 					<div key={result.id}>
+				// 						{result.poster_path &&
+				// 							<img src={`https://image.tmdb.org/t/p/w200${result.poster_path}`} alt=""/>
+				// 						}
+				// 						<h2>{result.title}</h2>
+				// 						<SimpleModal>
+				// 							<h3>{result.title}</h3>
+				// 							<p>{result.overview}</p>
+				// 						</SimpleModal>
+				// 					</div>
+				// 				))
+				// 			)
+				// 		}}
+				// 	</Async.Fulfilled>
+				// 	<Async.Rejected>
+				// 		{error => `Something went wrong: ${error.message}`}
+				// 	</Async.Rejected>
+				// </Async>
 			}
 		</div>
 	)
